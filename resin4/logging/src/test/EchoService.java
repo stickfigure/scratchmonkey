1d45411f3b564965a31ee4371c7ca83bfae6eb27
lhoriman <lhoriman@bd7b5c46-171e-11de-896d-b39e94f1936c>
2009-05-07 23:11:01 +0000

    Basic test of logging facilities

--- resin4/logging/src/test/EchoService.java
+++ resin4/logging/src/test/EchoService.java
@@ -1,29 +1,37 @@
 package test;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.caucho.config.Service;
 
 @Service
-public class EchoService {
-	private static final Logger log = Logger.getLogger(EchoService.class.getName());
+public class EchoService
+{
+	private static final Logger log = LoggerFactory.getLogger(EchoService.class);
 
 	@PostConstruct
-	public void start() {
-		log.log(Level.WARNING,"Starting EchoService Service!");
+	public void start()
+	{
+		log.trace("Starting @ level trace");
+		log.debug("Starting @ level debug");
+		log.info("Starting @ level info");
+		log.warn("Starting @ level warn");
+		log.error("Starting @ level error");
 	}
 
 	@PreDestroy
-	public void stop() {
-		log.log(Level.WARNING,"Stopping EchoService Service!");
+	public void stop()
+	{
+		log.debug("Stopping @ level debug");
 	}
 	
-	public String echo(String s){
-		log.log(Level.INFO,"Echoing: " + s);	
+	public String echo(String s)
+	{
+		log.debug("Echoing at level debug: " + s);	
 		return s;
 	}
 }

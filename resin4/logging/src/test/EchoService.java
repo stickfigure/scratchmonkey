6c8fbdca2ce62f4a0564f23f62e0e5cb798c603d
lhoriman <lhoriman@bd7b5c46-171e-11de-896d-b39e94f1936c>
2009-05-08 00:43:54 +0000

    Logging seems to be working as advertised.

--- resin4/logging/src/test/EchoService.java
+++ resin4/logging/src/test/EchoService.java
@@ -1,7 +1,8 @@
 package test;
 
+import java.util.logging.Level;
+
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,23 +13,26 @@ import com.caucho.config.Service;
 public class EchoService
 {
 	private static final Logger log = LoggerFactory.getLogger(EchoService.class);
+	private static final java.util.logging.Logger logJDK = java.util.logging.Logger.getLogger(EchoService.class.getName());
 
 	@PostConstruct
 	public void start()
 	{
-		log.trace("Starting @ level trace");
-		log.debug("Starting @ level debug");
-		log.info("Starting @ level info");
-		log.warn("Starting @ level warn");
-		log.error("Starting @ level error");
+		log.trace("Starting @ trace with slf4j");
+		log.debug("Starting @ debug with slf4j");
+		log.info("Starting @ info with slf4j");
+		log.warn("Starting @ warn with slf4j");
+		log.error("Starting @ error with slf4j");
+
+		logJDK.log(Level.FINEST, "Starting @ FINEST with jdk");
+		logJDK.log(Level.FINER, "Starting @ FINER with jdk");
+		logJDK.log(Level.FINE, "Starting @ FINE with jdk");
+		logJDK.log(Level.CONFIG, "Starting @ CONFIG with jdk");
+		logJDK.log(Level.INFO, "Starting @ INFO with jdk");
+		logJDK.log(Level.WARNING, "Starting @ WARNING with jdk");
+		logJDK.log(Level.SEVERE, "Starting @ SEVERE with jdk");
 	}
 
-	@PreDestroy
-	public void stop()
-	{
-		log.debug("Stopping @ level debug");
-	}
-	
 	public String echo(String s)
 	{
 		log.debug("Echoing at level debug: " + s);	

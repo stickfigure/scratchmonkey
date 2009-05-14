4003733f5e5620a99fc892fa57bda68f8585307d
lhoriman <lhoriman@bd7b5c46-171e-11de-896d-b39e94f1936c>
2009-05-14 20:09:26 +0000

    Stripped down to just hessian-related stuff.

--- resin4/hessian/src/test/EchoService.java
+++ resin4/hessian/src/test/EchoService.java
@@ -1,11 +1,7 @@
 package test;
 
-import java.security.Principal;
 import java.util.logging.Logger;
 
-import javax.annotation.Resource;
-import javax.annotation.security.RolesAllowed;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 
 @Stateless
@@ -13,15 +9,9 @@ public class EchoService implements Echo
 {
 	private static final Logger log = Logger.getLogger(EchoService.class.getName());
 
-	@Resource SessionContext context;
-	
-	@RolesAllowed("user")
 	public String echo(String s)
 	{
 		log.info("Echoing: " + s);
-		
-		Principal p = context.getCallerPrincipal();
-		log.info("Caller principal is: " + p.getName());
 		
 		return s;
 	}

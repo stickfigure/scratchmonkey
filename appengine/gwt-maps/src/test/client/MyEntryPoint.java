189dea717cf5dfdcbf2d5a79b816afe17c0e50e9
lhoriman <lhoriman@bd7b5c46-171e-11de-896d-b39e94f1936c>
2009-12-12 19:12:38 +0000

    Trivial test of map placement.

--- appengine/gwt-maps/src/test/client/MyEntryPoint.java
+++ appengine/gwt-maps/src/test/client/MyEntryPoint.java
@@ -1,6 +1,11 @@
 package test.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,5 +17,15 @@ public class MyEntryPoint implements EntryPoint
 	 */
 	public void onModuleLoad()
 	{
+		DockLayoutPanel dl = new DockLayoutPanel(Unit.EM);
+		
+		LatLng cawkerCity = LatLng.newInstance(39.509, -98.434);
+
+		MapWidget map = new MapWidget(cawkerCity, 4);
+		map.setSize("100%", "100%");
+		
+		dl.add(map);
+		
+		RootLayoutPanel.get().add(dl);
 	}
 }

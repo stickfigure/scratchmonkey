706392c4efe6c3b45182c6f9dac64e7c73041e75
lhoriman <lhoriman@bd7b5c46-171e-11de-896d-b39e94f1936c>
2010-04-07 03:28:11 +0000

    Basic test is now on 1.3.2

--- appengine/basic/src/test/AppEngineTestServlet.java
+++ appengine/basic/src/test/AppEngineTestServlet.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,9 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class AppEngineTestServlet extends HttpServlet
 {
+	/** */
+	private static final Logger log = Logger.getLogger(AppEngineTestServlet.class.getName());
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
 	{
 		resp.setContentType("text/plain");
+		
+		log.info("Executing doGet()");
+		
 
 //		String url = "http://voodoodyne.appspot.com/hello";
 

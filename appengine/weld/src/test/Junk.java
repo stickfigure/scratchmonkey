e94e882f703b2852ada482aa294f7035c652d61c
lhoriman <lhoriman@bd7b5c46-171e-11de-896d-b39e94f1936c>
2010-05-17 22:40:13 +0000

    Working on HttpParam injection.

--- appengine/weld/src/test/Junk.java
+++ appengine/weld/src/test/Junk.java
@@ -14,6 +14,8 @@ import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import unsuck.cdi.HttpParam;
+
 /**
  * Some random junk for client testing
  * 
@@ -23,10 +25,11 @@ import org.slf4j.LoggerFactory;
 public class Junk
 {
 	/** */
-	@SuppressWarnings("unused")
 	private final static Logger log = LoggerFactory.getLogger(Junk.class);
 
 	@Inject Thing thing;
+	
+	@Inject @HttpParam("asdf") String asdf;
 	
 	/**
 	 * Blah!
@@ -36,6 +39,8 @@ public class Junk
 	public String blah(@QueryParam("blah") String blah) throws Exception
 	{
 		this.thing.doit();
+		
+		log.info("asdf is " + asdf);
 		
 		return blah;
 	}
